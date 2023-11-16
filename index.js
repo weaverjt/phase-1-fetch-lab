aa2d2ec8a92e754f3c0991eaa1760227951d470d
Jacob <weaver.jacob.t@gmail.com>
2023-11-15 21:41:42 -0600

    done

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-function fetchBooks() {
+ function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    .then((json) => renderBooks(json));
 }
 
 function renderBooks(books) {
@@ -12,6 +14,8 @@ function renderBooks(books) {
   });
 }
 
+
+
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+ fetchBooks(); 
 });
